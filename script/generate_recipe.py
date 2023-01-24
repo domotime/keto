d026168136d14550d85c19fc93b3864743252bf9
domotime <uff.tecnico@domotime.com>
2023-01-24 13:44:08 +0100

    Working scraping

--- script/generate_recipe.py
+++ script/generate_recipe.py
@@ -2,7 +2,7 @@ import os
 import openai
 
 openai.organization = "org-6oEWKOYBDy8lAtQVU7rLDb2m"
-openai.api_key = "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"
+openai.api_key = "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"
 openai.Model.list()
 
 from recipes.models.tag import Tag
@@ -26,31 +26,43 @@ import json
 
 to_save = {'recipes': []}
 
+with open('json_recipes/recipes.json', 'r') as fp:
+    to_save = json.load(fp)
+
 def save_json():
     global to_save
     with open('json_recipes/recipes.json', 'w') as fp:
         json.dump(to_save, fp)
 
+done_recipes = []
+for recipe in to_save['recipes']:
+    done_recipes.append(recipe['id'])
 
 def generate_recipes_from_tag(tag):
     global openai
     recipes = random_recipes(tag)
 
     global to_save
+    global done_recipes
 
     recipe: Recipe
     for recipe in recipes:
         # create a completion
         try:
-            completion = openai.Completion.create(engine="text-davinci-003", prompt=\
-            ("Scrivi una ricetta in italiano di " + recipe.instructions), max_tokens=2000)
+            if recipe.pk in done_recipes:
+                print("Already saved")
+                continue
 
             title = openai.Completion.create(engine="text-davinci-003", prompt=\
                 ("Traduci in italiano " + recipe.name), max_tokens=2000)
 
+            
+            completion = openai.Completion.create(engine="text-davinci-003", prompt=\
+            ("Scrivi una ricetta in italiano di " + recipe.instructions), max_tokens=2000)
+
             # Generating image
             response = openai.Image.create(
-                prompt="Photo of " + recipe.name,
+                prompt=" good lightning tasty dish " + recipe.name + " with visual from above for a blog post", 
                 n=1,
                 size="1024x1024",
             )
@@ -64,7 +76,8 @@ def generate_recipes_from_tag(tag):
                 'name': title.choices[0].text,
                 'description': completion.choices[0].text,
                 'image': response["data"][0]["url"],
-                'tags': tags_to_return
+                'tags': tags_to_return,
+                'id': recipe.pk
             })
 
             save_json()
@@ -78,10 +91,6 @@ def generate_recipes_from_tag(tag):
 
 generate_recipes_from_tag('poultry')
 generate_recipes_from_tag('chicken')
-generate_recipes_from_tag('poultry')
-generate_recipes_from_tag('chicken')
-generate_recipes_from_tag('poultry')
-generate_recipes_from_tag('chicken')
 generate_recipes_from_tag('seafood')
 generate_recipes_from_tag('beef')
 generate_recipes_from_tag('pork')
@@ -91,6 +100,9 @@ generate_recipes_from_tag('eggs')
 generate_recipes_from_tag('fish')
 generate_recipes_from_tag('onion')
 generate_recipes_from_tag('tomatoes')
+
+exit()
+
 generate_recipes_from_tag('carrot')
 generate_recipes_from_tag('mushrooms')
 generate_recipes_from_tag('zucchini')
