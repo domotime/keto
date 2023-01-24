6af431b649ed1742fb0c52f3f4e2963ae414fbe4
domotime <uff.tecnico@domotime.com>
2023-01-24 20:13:50 +0100

    commit

--- script/generate_recipe.py
+++ script/generate_recipe.py
@@ -2,7 +2,7 @@ import os
 import openai
 
 openai.organization = "org-hap0lN10kL9bXjWOfqoYjjLj"
-openai.api_key = "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"
+openai.api_key = "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"
 openai.Model.list()
 
 from recipes.models.tag import Tag
@@ -112,7 +112,7 @@ def create_recipe_json(recipe: Recipe):
 
 # recipes = random_recipes("poultry") | random_recipes("chicken") | random_recipes("seafood") | random_recipes("beef") | random_recipes("pork") | random_recipes("salmon") | random_recipes("shrimp") | random_recipes("eggs") | random_recipes("fish") | random_recipes("onions") | random_recipes("tomatoes") | random_recipes("carrots") | random_recipes("mushrooms") | random_recipes("zucchini") | random_recipes("spinach") | random_recipes("bacon") | random_recipes("cheese") | random_recipes("potatoes") | random_recipes("nuts") | random_recipes("avocado")
 
-recipes = random_recipes("onions")
+recipes = random_recipes("tomatoes")
 
 import time
 
